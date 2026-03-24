6cd48561b07a2c5b34f724d4ea6b4df58640e322
vivekchdh0691-wq <vivekchdh0691@gmail.com>
2026-03-24 21:23:27 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -163,9 +163,15 @@ Send Enquiry
 
 <div className="mt-16 rounded-2xl overflow-hidden shadow">
 
-<iframe 
-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d218.90118323519619!2d77.02795735637936!3d28.617203811065167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d054605bffd5f%3A0x6d6a7705ebbace09!2sManoj%20R%20Kumar%20%26%20Co.!5e0!3m2!1sen!2sin!4v1774346890009!5m2!1sen!2sin" width="100%" height="350" style={{ border: 0 }} loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+<div className="mt-16 rounded-2xl overflow-hidden shadow">
+  <iframe
+    src="https://maps.google.com/maps?q=Kakrola%20Housing%20Complex%20New%20Delhi&output=embed"
+    width="100%"
+    height="350"
+    style={{ border: "0" }}
+    loading="lazy"
+  ></iframe>
+</div>
 </div>
 
 
